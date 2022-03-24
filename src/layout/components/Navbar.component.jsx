d643db32ae5c996057ea37564c733634e040249a
Usama Tufail <miansamusama@gmail.com>
2022-03-24 16:14:01 +0500

    feat(navbar): theme color change integrated in navbar

--- src/layout/components/Navbar.component.jsx
+++ src/layout/components/Navbar.component.jsx
@@ -1,8 +1,10 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Drawer, Button } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
-import './Navbar.styles.scss';
+import { useSelector } from 'react-redux';
 import { useState } from 'react';
+import { useHover } from 'hooks';
+import './Navbar.styles.scss';
 
 const links = [
   { path: '/dashboard/onsite-users', text: 'On-Site Users' },
@@ -14,6 +16,9 @@ const links = [
 
 export function Navbar() {
   const [visible, setVisible] = useState(false);
+  const [linkHovered, setLinkHovered] = useState(false);
+
+  const toggleHover = ({ path }) => setLinkHovered(path);
 
   const showDrawer = () => {
     setVisible(true);
@@ -23,24 +28,36 @@ export function Navbar() {
   };
 
   const { pathname } = useLocation();
+  const { primaryColor } = useSelector((state) => state.theme);
+
+  const linkHoverStyles = {
+    color: primaryColor,
+    border: `1px solid ${primaryColor}`
+  };
+
   return (
     <div className="navbar">
-      <div className="navbar__logo">
+      <div className="navbar__logo" style={{ background: primaryColor }}>
         <img src="/img/logo.png" alt="logo" />
       </div>
 
       <div className="navbar__links">
         <div className="navbar__links-container">
-          {links.map((link) => (
-            <Link
-              key={link.path}
-              to={link.path}
-              className={`navbar__links-container-el ${
-                pathname === link.path ? 'navbar__links-container-el-active' : ''
-              }`}>
-              {link?.text}
-            </Link>
-          ))}
+          {links.map((link) => {
+            return (
+              <Link
+                key={link.path}
+                to={link.path}
+                className={`navbar__links-container-el ${
+                  pathname === link.path ? 'navbar__links-container-el-active' : ''
+                }`}
+                style={linkHovered === link.path || pathname === link.path ? linkHoverStyles : {}}
+                onMouseEnter={() => toggleHover(link)}
+                onMouseLeave={() => setLinkHovered(false)}>
+                {link?.text}
+              </Link>
+            );
+          })}
         </div>
         <div className="navbar__links-right">
           <div className="navbar__links-right-lc">Live Count : 100</div>
